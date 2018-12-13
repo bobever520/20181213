821766aff081c86251479c2fb89c3aa5d8d58ceb
bobever <bobever520@gmail.com>
2018-12-13 23:36:43 +0800

    12

---
+++ 6.36/source/main.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+int main(void)
+{
+	char s[20];
+
+	printf("Please enter the strings\n");
+	getchar(s);
+
+	printf("the inverted strings order is\n");
+
+	printf("a p p l e\n");
+
+
+	system("pause");
+	return 0;
+}
